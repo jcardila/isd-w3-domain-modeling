de5c7c2cc79752d75b9835685ae9a6369965e113
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-01-21 04:40:00 +0000

    update

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,10 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+salespersons (id, first_name, last_name, email)
+contacts (id, name, company, email, phone_number)
+activities (id, salesperson_id, contact_id, activity_type, datetime, notes)
+companies (id, name)
+industries (id, name)
+industryMembership (id, industry_id, company_id)
