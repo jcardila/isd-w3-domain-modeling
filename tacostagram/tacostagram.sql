a9c2b7bcdbfa595f5dc5b075858ffb256e205d36
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-01-21 14:26:02 +0000

    fin clase

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,7 @@
+--https://entr451.com/domain-modeling-case-study-ii/
 
+users (id, screen_name, real name, location, profile_pic_url)
+posts (id, user_id_ image_url, posted_at)
+likes (id, post_id UNIQUE KEY, liker_user_id UNIQUE KEY)
+comments (id, post_id, commenter_user_id, comment, posted_at)
+follows (id, followed_user_id, follower_user_id)
